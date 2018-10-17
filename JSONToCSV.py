d640defb9ae11620dc6efeae90a585d752ddcf4a
cloudPai <1565730756@qq.com>
2018-10-17 14:34:25 +0800

    完成爬虫：获取scratch上某用户发布的所有作品的详细信并存储为csv文件

--- JSONToCSV.py
+++ JSONToCSV.py
@@ -1,16 +1,22 @@
 import csv
 import json
 
-f = open('1.json',"r",encoding="utf8")
-data = json.load(f)
-f.close()
 
 
-fcsv = csv.writer(open("1.csv", "w", encoding="utf8"))
-# Write CSV Header, If you dont need that, remove this line
-fcsv.writerow(["id","title","description","instructions","visibility","is_published","author_id","author_username","image","history_created","history_modified","history_shared","stats_views","stats_loves","stats_favorites","stats_comments","stats_remixes","remix_parent","remix_root"])
-for x in data:
-    fcsv.writerow([x["id"],
+
+def startSave(saveName):
+    f = open('1.json',"r",encoding="utf8")
+    data = json.load(f)
+    f.close()
+    fcsv = csv.writer(open(saveName, "w", encoding="utf8"))
+    # Write CSV Header, If you dont need that, remove this line
+    fcsv.writerow(["No.","id","title","description","instructions","visibility","is_published","author_id","author_username","image","history_created","history_modified","history_shared","stats_views","stats_loves","stats_favorites","stats_comments","stats_remixes","remix_parent","remix_root"])
+    i = 1
+    for x in data:
+        fcsv.writerow(
+            [
+                i,
+                x["id"],
                 x["title"],
                 x["description"],
                 x["instructions"],
@@ -34,4 +40,5 @@ for x in data:
 
                 x["remix"]["parent"],
                 x["remix"]["root"]
-                ])
+             ])
+        i +=1
--- saveJSON.py
+++ saveJSON.py
@@ -10,23 +10,24 @@ import json
 # rawtext = urlopen(fullurl, timeout=15).read()
 # jsonStr = json.loads(rawtext.decode('utf8'))
 
-str1 = "["
-for x in range(0, 5):
+def startDownload(username,rangeNum):
+    str1 = "["
+    for x in range(0, rangeNum):
+        url = "https://api.scratch.mit.edu/users/"+username +"/projects?limit=32&offset="+ str(x * 32)
+        # url="https://api.scratch.mit.edu/users/atomicmagicnumber/projects?limit =32&offset="+str(x*32)
+        print("x:", x,url)
+        r=requests.get(url)
+        str2 = str(r.text)[1:-1] # 去掉返回结果两边的中括号
+        print("str2",str2)
+        if(str2 == ""):
+            break
+        str1 += str2 + ","
 
-    url="https://api.scratch.mit.edu/users/atomicmagicnumber/projects?limit =32&offset="+str(x*32)
-    print("x:", x,url)
-    r=requests.get(url)
-    str2 = str(r.text)[1:-1] # 去掉返回结果两边的中括号
-    print("str2",str2)
-    if(str2 == ""):
-        break
-    str1 += str2 + ","
+    str1 = str1[:-1]+"]"
+    print("str1",str1)
+    # 将ltp处理结果保存到文本中
 
-str1 = str1[:-1]+"]"
-print("str1",str1)
-# 将ltp处理结果保存到文本中
-
-# url="https://api.scratch.mit.edu/users/atomicmagicnumber/projects?limit =32&offset="+str(0*32)
-f = open("1.json", "w", encoding="utf8")
-f.write(str1)  # 保存前，需要将jsonStr序列化为python相对的数据类型，去掉最后的换行符
-f.close()
+    # url="https://api.scratch.mit.edu/users/atomicmagicnumber/projects?limit =32&offset="+str(0*32)
+    f = open("1.json", "w", encoding="utf8")
+    f.write(str1)  # 保存前，需要将jsonStr序列化为python相对的数据类型，去掉最后的换行符
+    f.close()
